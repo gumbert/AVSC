65eccb719c0e684915dcec16fe8dadc8b7b67b28
gumbert <gumbertello@gmail.com>
2014-06-06 15:47:04 +0400

    in features just category, company, brand, month and week in test data

--- csvTest.py
+++ csvTest.py
@@ -1,25 +1,43 @@
 import numpy as np
 import time
+from datetime import datetime as dt
 
 def main():
 	try:
-		testname = "data\\outputTestAll_v9.csv"
-		mat = outtest(testname)
-		mat = zerorows(mat)
+		testname = "data\\outputTestAll.csv"
+		histname = "data\\testHistory.csv"
+		mat, mat1 = outtest(testname, histname)
+		mat = zerorows(mat, mat1)
+		mat = meannorm(mat)
 		printtest(mat, "data\\outTest-05+05.txt")		
 	
 	except Exception, e:
 		print str(e)
 		
-def meannorm(arr):
-	min = np.amin(arr)
-	max = np.amax(arr)
-	av = arr.sum()/len(arr)
-	if max == 0 and min == 0:
-		arr = 0
-	else:
-		arr = (arr-av)/(max-min)
-	return arr
+def meannorm(mat):	
+	for i in range(len(mat[:,0])):
+		arr = mat[i,:]
+		# print "before", arr
+		min = np.amin(arr)
+		max = np.amax(arr)
+		av = arr.sum()/len(arr)
+		if max == 0 and min == 0:
+			arr = np.zeros(arr.shape)
+		else:
+			arr = (arr-av)/(max-min)
+		mat[i,:] = arr
+		# print "after", arr
+	return mat
+	
+# def meannorm(arr):
+	# min = np.amin(arr)
+	# max = np.amax(arr)
+	# av = arr.sum()/len(arr)
+	# if max == 0 and min == 0:
+		# arr = 0
+	# else:
+		# arr = (arr-av)/(max-min)
+	# return arr
 	
 def printtest(mat, fileout):
 	fo = open(fileout, 'w')
@@ -33,53 +51,81 @@ def printtest(mat, fileout):
 	fo.close()
 
 
-def outtest(fname):
-	with open(fname) as f:
+def outtest(fname, fname1):
+	with open(fname) as f, open(fname1) as f1:
 		f.next()
-		#id,chain,offer,market,repid,sumid,avid,repchain,sumchain,avchain,repcategory,sumcategory,avcategory,repcompany,sumcompany,avcompany,repbrand,sumbrand,avbrand,month,repmonth,summonth,avmonth,weekday,repweekday,sumweekday,avweekday = np.loadtxt(f, delimiter=',', unpack='true', dtype='float')
+		f1.next()
 
 		mat = np.loadtxt(f, delimiter=',', unpack='true', dtype='float')
-		#print len(mat)
-			
-		#for i in range(2, len(mat[:,0])):
-		#	mat[i,:] = meannorm(mat[i,:])
-		#print mat
-		return mat
+		mat1 = np.loadtxt(f1, delimiter=',', unpack='true', dtype='str')
 		
-def zerorows(mat):
+		#print len(mat)		
+		return mat, mat1
+		
+def zerorows(mat, mat_test):
+	fname_test = "data\\testHistory.csv"
 	fname_off = "data\\offers.csv"
 	with open(fname_off) as f:
 		f.next()
+
 		mat_off = np.loadtxt(f, delimiter=',', unpack='true', dtype='int')
 		
 		unique_category = np.unique(mat_off[1,:])
-		print "category", len(unique_category)
+		# print "category", len(unique_category)
 		unique_company = np.unique(mat_off[3,:])
+		# print "company", len(unique_company)
 		unique_brand = np.unique(mat_off[5,:])
+		# print "brand", len(unique_brand)
 		
 		#print "len offer file", len(mat_off)
 		for i in range(len(mat[0,:])):
 			idx = np.where(mat_off[0,:]==mat[3,i])
-			cat = mat_off[1,idx]
-			comp = mat_off[3,idx]
+			category = mat_off[1,idx]
+			company = mat_off[3,idx]
 			brand = mat_off[5,idx]
 			
+			date = mat_test[4,i]
+			# print "date", date
+			
+			month, weekday = dat(date)
+			
 			# category
-			idxcat = np.where(unique_category==cat)
-			idx1 = 11
-			idx2 = (len(unique_category))*3+idx1
-			tmat = np.reshape(mat[idx1:idx2,i],(-1,3))
-			# print tmat
-			tarr = tmat[idxcat[1],:]
-			tmat = np.zeros(tmat.shape)
-			tmat[idxcat[1],:] = tarr
-			# print mat
-			mat[idx1:idx2,i] = np.reshape(tmat,idx2-idx1)
-			print idx1, idx2, idxcat[1], tarr
-			# print "iter", i, idxcat, tmat, tmat[idxcat[1],:], tarr
-				
+			# mat = zerocategory(mat, unique_category, cat, i)
+			mat = zero(mat, unique_category, category, 11, 71, i)
+			
+			# company
+			# mat = zerocompany(mat, unique_company, comp, i)
+			mat = zero(mat, unique_company, company, 71, 125, i)
+			
+			# brand
+			mat = zero(mat, unique_brand, brand, 125, 182, i)
+			
+			# month
+			mat = zero(mat, np.arange(1,13), month, 182, 218, i)
+			
+			# weekday
+			mat = zero(mat, np.arange(1,8), weekday, 218, 239, i)
+			
 	return mat
 
+def dat(date):
+	d = dt.strptime(date, '%Y-%m-%d').date()
+	month = d.month
+	weekday = d.isoweekday()	
+	return month, weekday
+	
+def zero(mat, unique, feature, idx1, idx2, i):
+	idxcat = np.where(unique==int(feature))
+	tmat = np.reshape(mat[idx1:idx2,i],(-1,3))
+	# print tmat, type(unique), unique, type(feature), feature, idxcat
+	tarr = tmat[idxcat,:]
+	tmat = np.zeros(tmat.shape)
+	tmat[idxcat,:] = tarr
+	# print mat
+	mat[idx1:idx2,i] = np.reshape(tmat,idx2-idx1)
+	# print idx1, idx2, idxcat, tarr
+	# print "iter", i, idxcat, tmat, tmat[idxcat[1],:], tarr
+	return mat
 
 start_time = time.time()
 main()
